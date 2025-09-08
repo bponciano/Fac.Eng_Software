f542abe6a87d848f03eb78a7b3f18692b15f1a96
Breno Ponciano <brenorodrigues37@gmail.com>
2025-09-08 16:13:38 +0000

    estudo

--- Indrmento_decremento.c
+++ Indrmento_decremento.c
@@ -3,19 +3,33 @@
 int main() {
 /*
 Incremento (++)
-Pré Incremento++a
+Pré Incremento ++a
 Decremendot (--)
 Pré Decremento --a
 Pós Decremento a--
 */
 
-int numero1 = 1;
+int numero1 = 1, resultado;
 
 printf("Antes do incremento: %d\n", numero1);
+
+// numero1 = numero1 + 1;
+
 numero1++;
 printf("Após incremento: %d\n", numero1);
 
 numero1--;
 printf("Após decremento: %d\n", numero1);
 
+//Pós incremento
+//resultado = numero1;
+//numero++;
+resultado = numero1++;
+printf("Após Pós incremento - Numero 1: %d - Resultado %d\n", numero1, resultado);
+
+//Pre incremento
+//numero1++;
+//rusultado = numero1;
+resultado = ++numero1;
+printf("Após Pre incremento - Numero 1: %d - Resultado %d\n", numero1, resultado);
 }
