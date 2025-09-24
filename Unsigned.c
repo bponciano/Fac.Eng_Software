e81584c98f502fb7a802f4aae774b26d365eefb9
Breno Ponciano <brenorodrigues37@gmail.com>
2025-09-24 16:25:33 +0000

    Criação Arquivo Unsigned

---
+++ Unsigned.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    unsigned long int numerograndepositivo = 4000000000;
+    unsigned int numeropositivo = 4000000000;
+    long int numerogrande = 4000000000;
+    int numero = 4000000000;
+
+    printf("Número positivo grande: %lu\n", numerograndepositivo);
+    printf("Número positivo: %u\n", numeropositivo);
+    printf("Número grande: %ld\n", numerogrande);
+    printf("Número: %d\n", numero);
+    return 0;
+}
