757891999e6490bc302a45b604558a4accf43ece
Breno Ponciano <brenorodrigues37@gmail.com>
2025-09-24 16:30:34 +0000

    Criação do Arquivo Short

---
+++ Short.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    short int numeropequeno = 32767; // Valor maximo de short
+    printf("Numero pequeno (short int): %d\n", numeropequeno);
+
+    short int numeropequeno = 32768;
+    printf("Numero pequeno (short int): %d\n", numeropequeno);
+
