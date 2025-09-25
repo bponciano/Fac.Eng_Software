5d48311d5d7db460a6f4a96925118d791ce794ab
Breno Ponciano <brenorodrigues37@gmail.com>
2025-09-25 16:21:33 +0000

    Criação do Arquivo Variaveis_operadores

---
+++ Variaveis_operadores.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    float numero1 = 10.2;
+    int numero2 = 10;
+
+    printf("Numero1 > numer02: %d\n", (int)numero1 > numero2);
+    printf("Numero1 == numer02: %d\n", (int)numero1 == numero2);
+    return 0;
+}
